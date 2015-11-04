75341aef07592ca123e96744fe8d218325545cdc
sonofizanagi <stanimir.ivanov@gmail.com>
2015-11-04 17:59:04 +0000

    Complete assignment

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,67 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  return 0 if arr.empty?
+  arr.reduce :+
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  return 0 if arr.empty?
+  return arr[0] if arr.length == 1
+  sum(arr.sort.last(2))
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  if arr.length < 2
+    return false
+  else
+    arr.combination(2).to_a.each do |x, y|
+      return true if x + y == n
+    end
+  end
+  false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  !!(s[0] =~ /[bcdfghjklmnprstvwxyz]+/i)
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s =~ /^[0-1]+$/
+    return s.to_i(2) % 4 == 0 unless s.length == 0
+  end
+  false
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+
+  def initialize isbn, price
+    raise ArgumentError if isbn.empty?
+    raise ArgumentError if price <= 0
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    format("$%.2f", @price)
+  end
+end
+
+def mystery_sequence(start1, start2, limit=4)
+  yield start1
+  yield start2
+  nextval = start1 + start2
+  1.upto(limit) do
+    yield nextval
+    nextval, start2 = nextval + start2, nextval
+  end
 end
